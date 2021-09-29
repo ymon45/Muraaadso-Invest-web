5993dbd7c5ee339e39b9e7c03c5abc28344562a5
ymon45 <yayebinumoon@gmail.com>
2021-09-29 09:51:11 +0300

    perfomance change

--- src/Components/Perfomance.js
+++ src/Components/Perfomance.js
@@ -11,14 +11,14 @@ const Perfomance = () => {
             <section class="container perfomsec">
   <div class="container px-5 py-3 percrd ">
     <div class="flex flex-wrap -m-4 text-center items-center justify-center contsec">
-      <div class="p-4 md:w-2/4  w-full perp ">
+      <div class="p-4 lg:w-1/4 md:w-2/4  w-full perp ">
         <div class="border-2 border-gray-800 px-4 py-6 cardd-1">          
           <h2 class="title-font font-medium ">Annual Return</h2>
           <p class="leading-relaxed">60%</p>
         </div>
       </div>
       
-      <div class="p-4 md:w-2/4 sm:w-1/2 w-full perp ">
+      <div class="p-4 lg:w-1/4 md:w-2/4 sm:w-1/2 w-full perp ">
         <div class="border-2 border-gray-800 px-4 py-6 cardd-2">
           <h2 class="title-font font-medium ">Quarterly Return </h2>
           <p class="leading-relaxed">12.25%</p>
